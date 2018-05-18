9957c2d66a5d97d55ae0429c06bbfc5552e13fc1
Irhum Shafkat <13271155+irhum@users.noreply.github.com>
2018-05-18 17:50:08 +0600

    First commit of the implementation

---
+++ request.py
@@ -0,0 +1,23 @@
+import requests
+import argparse
+
+arg_p = argparse.ArgumentParser()
+arg_p.add_argument("-i", "--image", required=True, 
+                    help="Name of the image file to be classified") 
+
+args = vars(arg_p.parse_args()) 
+
+REST_API_URL = "http://localhost:5000/predict"
+IMAGE_PATH = args["image"]
+
+image = open(IMAGE_PATH, "rb").read()
+payload = {"image": image}
+
+r = requests.post(REST_API_URL, files=payload).json()
+
+if r["success"]:
+    for (i, result) in enumerate(r["predictions"]):
+        print("{}. {}: {:.4f}".format(i + 1, result["label"],
+            result["probability"]))
+else:
+    print("Failed request")
